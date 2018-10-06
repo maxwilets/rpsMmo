5a3985dd28773327c1cafd68d09d7ff64a9c54e2
Max Wilets <mwilet@gmail.com>
2018-10-06 10:45:51 -0600

    rpsMmo

--- assets/game.js
+++ assets/game.js
@@ -7,89 +7,79 @@ $(document).ready(function(){
         storageBucket: "rockpaper-f90b6.appspot.com",
         messagingSenderId: "103007121094"
       };
-      var playerName;
-      playerz = 0;
-
+     
       firebase.initializeApp(config);
-      var database = firebase.database();
-       userName = 0
-      var player = {
-          player1: {
-              name: 0,
-              play: 1
-          },
-        
-          player2:{
-              name: 0,
-              play: 1
-
-          },
-          plays: {
-              myTurn: 1,
-              opTurn: 1
-          },
-          wins: {
-              myWins: 0,
-              yourWin: 0,
-              ties: 0
-          },
-          oppName: null
-      };
-
+      var database = firebase.database(); 
       
-        nameUp = function(){
-            if (player.player1.name === 0){
-            $('#add-user').on('click', function(){
-                console.log('cat')
-                playerz++;
-                userName = $('#name-input').val().trim();
-                console.log(userName);
-                player.player1.name = userName;
-                console.log(player.player1.name)
-               database.ref().set({
-                    player1: {
+    var win1 = 0;
+    var loss1 = 0;
+    var win2 = 0;
+    var loss2 = 0;
+    userName = '';
+   var player1;
+   var player2;
+
+    $('#add-user').on('click', function(){
+        var userName = $('#name-input').val().trim();
+        console.log('test')
+
+        database.ref().once('value').then(function(snapshot){
+
+            if((snapshot.child('player').child(1).exists()) ===false) {
+                database.ref('player/1').set({
+                    name: userName,
+                   win: win1,
+                   loss: loss1
+                });
+                $('#top').empty();
+                $('#top').html('<h3> Welcome, ' +userName + ' to Knight, Ninja Mage!' + '<h3>' + '<p> wins: ')
+                player1 = true;
+           
+            }
+            else if((snapshot.child('player').child(1).exists()=== true) &&
+               ((snapshot.child('player').child(2).exists()=== false))){
+                   database.ref('player/2').set({
                        name: userName,
-                       play: 0
-                    }
-                   }
-               )
-            })
-        }
-         else if((player.player1.name !== 0) && (player.player.name ===0)){
-             $('#add-user').on('click', function(){
-                 console.log('dog')
-                 userName= $('#name-input').val().trim();
-                 database.ref().set({
-                     player2: {
-                         name: userName,
-                         play: 0
-                     }
-                 })
-             })
-         }  
-        }
-      
-     console.log('test1')
-     var connectionsRef = database.ref('/connection');
-
-     var connectedRef = database.ref('.info/connected');
-
- //    connectedRef.on('value', function(snap) {
- //        if (snap.val()) {
- //            var con = connectionsRef.push(true);
-
- //            con.onDisconnect().remove();
- //        }
- //    });
- //     connectionsRef.on('value', function(snap){
- //      numberP = (snap.numChildren());
- //       $('#you').text(numberP);
-  //    });
-  //  
-
-     
-     
+                       win: win2,
+                       loss: loss2
+                   })
+                   $('#top').empty();
+                   player1 = false
+                $('#top').html('<h3> Welcome, ' +userName + ' to Knight, Ninja Mage!' + "<p> You're opponet is" + snapshot.child('player/1').val().name +'<h3>' )
+               }
+               else if ((snapshot.child('player').child(1).exists()) && (snapshot.child('player').child(2).exists())){
+                   alert('too many people in the game room')
+               }
+        })
+      console.log(player1)
    
+        })
+        
   
-   nameUp();
+      
+      
+    database.ref().on('value', function(snapshot){
+
+        var disconnect = function(){
+            if(userName != '') {
+                if ((snapshot.child('player').child(1).exists()) && (userName == snapshot.child('player').child(1).val().name)){
+                    database.ref('/chat').onDisconnect().update({
+                        message: '',
+                        dateAdded: ''
+                    })
+                database.ref('player/1').onDisconnect().remove();
+
+        
+                }
+                else if((snapshot.child('players').child(2).exists()) && (userName == snapshot.child('player').chid(2).val().name)){
+                    database.ref('/chat').onDisconnect().update({
+                        message: '',
+                        dateAdded: ''
+
+                    });
+                }
+            }
+        }
+    })
+
     })
